0b7eea404fa993947b288d037654889bd135017c
Victor Valenzuela <victor.valenzuelalobos@example.com>
2024-05-26 20:14:42 +0200

    Add filter

--- src/Pages/Inicio.js
+++ src/Pages/Inicio.js
@@ -4,7 +4,7 @@ import { db } from "../firebase";
 import { UserAuth } from "../Components/AuthContext";
 import { useNavigate } from "react-router-dom";
 import Loader from "../Components/loader";
-import { Button, Grid, Card, CardContent, Typography, CardActions, Box } from "@mui/material";
+import { Button, Grid, Card, CardContent, Typography, CardActions, Box, Select, MenuItem, InputLabel, FormControl } from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
 import ModalElement from "../Components/ModalCard";
@@ -13,10 +13,10 @@ function Inicio() {
   const { user } = UserAuth();
   const [cards, setCards] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [selectedCard, setSelectedCard] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [searchTerm, setSearchTerm] = useState('');
+  const [wordTypeFilter, setWordTypeFilter] = useState(''); // Estado para el tipo de palabra
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -59,19 +59,18 @@ function Inicio() {
   };
 
   const handleOpenModal = (card, index) => {
-    setSelectedCard(card);
     setSelectedIndex(index);
     setIsModalOpen(true);
   };
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
-    setSelectedCard(null);
     setSelectedIndex(0);
   };
 
   const filteredCards = cards.filter(todo =>
-    todo.rootWord.toLowerCase().startsWith(searchTerm.toLowerCase())
+    todo.rootWord.toLowerCase().startsWith(searchTerm.toLowerCase()) &&
+    (wordTypeFilter === '' || todo.wordType === wordTypeFilter)
   );
 
   if (loading) {
@@ -97,18 +96,30 @@ function Inicio() {
 
   return (
     <>
-      <div style={{ display: "flex", justifyContent: "center" }}>
+       <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", gap: 2, marginTop: 2, marginBottom: 2 }}>
         <input
           type="text"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Wort suchen..."
-          style={{ marginTop: 20, marginBottom: 20, borderRadius: 5, paddingLeft: 10 }}
+          style={{ borderRadius: 5, paddingLeft: 10, height:40, width:140 }}
         />
-      </div>
-      <Grid container spacing={3} justifyContent="center"
-      style={{paddingBottom: 20}}
-      >
+        <FormControl size="small" variant="outlined" style={{ width:140 }}>
+          <InputLabel id="word-type-filter-label">Filter</InputLabel>
+          <Select
+            labelId="word-type-filter-label"
+            value={wordTypeFilter}
+            onChange={(e) => setWordTypeFilter(e.target.value)}
+            label="Filter"
+          >
+            <MenuItem value=""><em>Alles</em></MenuItem>
+            <MenuItem value="Nomen">Nomen</MenuItem>
+            <MenuItem value="Verb">Verb</MenuItem>
+            <MenuItem value="Adverb/Adjektiv">Adverb/Adjektiv</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <Grid container spacing={3} justifyContent="center" style={{ paddingBottom: 20 }}>
         {filteredCards.map((todo, index) => (
           <Grid key={todo.uuid} item xs={5.5} sm={4} md={3.5} lg={2.5}>
             <Card variant="elevation" elevation={5} onClick={() => handleOpenModal(todo, index)}>
